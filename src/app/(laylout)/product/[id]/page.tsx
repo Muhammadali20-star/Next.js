f17b27a093982ea46ecf4e64027356d3bbe1909e
Muhammadali <muhamadalimamadaminov@gmail.com>
2025-07-19 15:18:31 +0300

    .

--- src/app/(laylout)/product/[id]/page.tsx
+++ src/app/(laylout)/product/[id]/page.tsx
@@ -1,16 +1,17 @@
 import ProductDetails from '@/components/products-detail/ProductDetails';
-import React from 'react'
+import React from 'react';
 
-const ProductDetail = async ({params}: {params: {id: string}}) => {
-    const id = params.id 
+const ProductDetail = async ({ params }: { params: { id: string } }) => {
+  const id = params.id;
 
-    const data = await fetch(`https://dummyjson.com/products/${id}`, { cache: "force-cache" });
-    const product = await data.json();
+  const data = await fetch(`https://dummyjson.com/products/${id}`, { cache: "force-cache" });
+  const product = await data.json();
+
   return (
     <div>
-        <ProductDetails data={product}/>
+      <ProductDetails data={product} />
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
