3b53049e33266cecb19c5796acef7e2f86067d6c
Muhammadali <muhamadalimamadaminov@gmail.com>
2025-07-19 18:16:41 +0300

    .

--- src/app/(laylout)/product/[id]/page.tsx
+++ src/app/(laylout)/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 import ProductDetails from '@/components/products-detail/ProductDetails';
 import React from 'react';
 
-const ProductDetail = async ({ params }: { params: { id: string } }) => {
+const ProductDetail = async ({ params }: { params: { id: string }; }) => {
   const id = await params.id
   const data = await fetch(`https://dummyjson.com/products/${id}`, { cache: "force-cache" });
   const product = await data.json();
--- src/app/(laylout)/user/[id]/page.tsx
+++ src/app/(laylout)/user/[id]/page.tsx
@@ -1,7 +1,7 @@
 import UserDetails from '@/components/user-detail/UserDetails';
 import React from 'react'
 
-const UserDetail  = async ({params}: {params: {id: string}}) => { 
+const UserDetail  = async ({params}: {params: {id: string};}) => { 
   const id = await params.id
     const data = await fetch(`https://dummyjson.com/users/${id}`, { cache: "force-cache" });
     const user = await data.json();
